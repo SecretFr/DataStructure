8c95ee807a13753bb597a630f95cbde521887b70
SecretFr <linkclean@naver.com>
2021-07-17 17:37:53 +0900

    hashmap 정렬방법

--- src/_sort/Score.java
+++ src/_sort/Score.java
@@ -4,9 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.Map.Entry;
 
 
 //성적이 낮은 순서로 학생 출력하기
@@ -27,15 +31,30 @@ public class Score {
             map.put(st.nextToken(), Integer.parseInt(st.nextToken()));
         }
 
-        System.out.println("KeySet()"+map.keySet());
-        for(String key : map.keySet()){
-            System.out.println(String.format("key: %s, value: %s", key, map.get(key)));
-        }
+        // System.out.println("KeySet()"+map.keySet());
+        // for(String key : map.keySet()){
+        //     System.out.println(String.format("key: %s, value: %s", key, map.get(key)));
+        // }
 
-        List<Integer> valueList = new ArrayList<>(map.values());
-        valueList.sort(Integer::compareTo);
-        for(Integer value : valueList){
-            System.out.print(value+" ");
+        // List<Integer> valueList = new ArrayList<>(map.values());
+        // valueList.sort(Integer::compareTo);
+        // for(Integer value : valueList){
+        //     System.out.print(value+" ");
+        // }
+        
+        List<Entry<String, Integer>> listEntry = new ArrayList<Entry<String, Integer>>(map.entrySet());
+        
+        System.out.println("entry set: "+listEntry);
+        Collections.sort(listEntry, new Comparator<Entry<String, Integer>>() {
+            public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2){
+                System.out.println("obj1 : "+obj1);
+                System.out.println("obj2 : "+obj2);
+                return obj1.getValue().compareTo(obj2.getValue());
+            }
+        });
+
+        for(Entry<String, Integer> entry: listEntry){
+            System.out.print(entry.getKey()+" ");
         }
     }
 }
