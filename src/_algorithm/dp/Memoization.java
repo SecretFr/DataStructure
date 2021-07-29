f6c58ff240ac6c32dd9f2310b6b781f79f42a731
SecretFr <linkclean@naver.com>
2021-07-29 19:30:25 +0900

    top-down dp

--- src/_algorithm/dp/Memoization.java
+++ src/_algorithm/dp/Memoization.java
@@ -1,9 +1,10 @@
 package _algorithm.dp;
-
+//top-down 방식
 public class Memoization {
     public static long[] d = new long[100];
 
     public static long fibo(int x){
+        System.out.println("f("+x+") ");
         if(x==1 || x==2){
             return 1;
         }
@@ -19,6 +20,6 @@ public class Memoization {
 
     public static void main(String[] args){
         //한 번 계산된 결과를 메모이제이션하기 위한 배열 초기화
-        System.out.println(fibo(4));
+        System.out.println(fibo(6));
     }
 }
